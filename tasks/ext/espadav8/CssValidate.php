c01e4b69dbf1c7e88d24387d660c38a11f7aadca
Andrew Smith <espadav8@gmail.com>
2010-07-26 17:30:03 +1000

    Minor updates to the CSS validator task. Added consts for the valid values the validator accepts. Add tha start of handling HTML and Soap outputs. Look at using an XSL file to process the Soap.

--- tasks/ext/espadav8/CssValidate.php
+++ tasks/ext/espadav8/CssValidate.php
@@ -3,12 +3,24 @@ require_once "phing/Task.php";
 
 class CssValidate extends Task
 {
-    protected $_file = null;
-    protected $_validatorUrl = 'http://jigsaw.w3.org/css-validator/validator';
-    protected $_profile = 'css21';
+    static const HTML           = 'html';
+    static const SOAP           = 'soap12';
+    static const TEXT           = 'text';
 
-    private $_output = 'soap12';
-    private $_soapClient = null;
+    static const CSS1           = 'css1';    
+    static const CSS2           = 'css2';
+    static const CSS21          = 'css21';
+    static const CSS3           = 'css3';
+    
+    protected $_file            = null;
+    protected $_uri             = null;
+    protected $_validatorUrl    = 'http://jigsaw.w3.org/css-validator/validator';
+    protected $_profile         = self::CSS21;
+
+    private $_output            = self::SOAP;
+    private $_soapClient        = null;
+    private $_destDir           = null;
+    private $_xslFile           = 'cssvalidate.xsl';
 
     public function init()
     {
@@ -16,26 +28,41 @@ class CssValidate extends Task
 
     public function main()
     {
-        if( empty( $this->_file ) )
-        {
-            throw new Exception( 'Empty file.' );
-        }
-        else if( !file_exists( $this->_file ) )
-        {
-            throw new Exception( "File: " . $this->_file . " - doesn't exist." );
-        }
-        print( 'Trying to validate: ' . $this->_file . PHP_EOL );
-
         $params = array(
-            'text'      => file_get_contents( $this->_file ),
             'output'    => $this->_output,
             'profile'   => $this->_profile,
         );
+        if( $this->_file !== null ) {
+            print( 'Trying to validate: ' . $this->_file . PHP_EOL );
+            $params['text'] = file_get_contents( $this->_file );
+        }
+        else if( $this->_uri !== null ) {
+            print( 'Trying to URI: ' . $this->_uri . PHP_EOL );
+            $params['uri'] = $this->_uri;
+        } else {
+            throw new Exception( "File: " . $this->_file . " - doesn't exist." );
+        }
+        
         $post = http_build_query($params);
-
-        $xml = simplexml_load_file( $this->_validatorUrl . '?' . $post );
-
-        $this->processResponse( $xml );
+        
+        if( $this->_output == self::HTML )
+        {
+            $html = file_get_contents( $this->_validatorUrl . '?' . $post );
+            return $this->saveHtmlOutput( $html );
+        }
+        else if( $this->_output == self::SOAP )
+        {
+            $xml = simplexml_load_file( $this->_validatorUrl . '?' . $post );
+            return $this->processResponse( $xml );
+        }
+        else if( $this->_output == self::TEXT ) {
+            return false;
+        }
+    }
+    
+    private function saveHtmlOutput( $html )
+    {
+        return file_put_contents( 'validate-css.html', $html );
     }
 
     private function processResponse( $xml )
@@ -50,11 +77,53 @@ class CssValidate extends Task
         echo 'Errors: ' . (string)$errorCount[0] . PHP_EOL;
         echo 'Warnings: ' . (string)$warningCount[0] . PHP_EOL;
     }
+    
+    /**
+     * Returns the path to the XSL stylesheet
+     */
+    private function getStyleSheet()
+    {
+        $xslname = 'cssvalidate.xsl';
+        
+        if( $this->styleDir )
+        {
+            $file = new PhingFile( $this->styleDir, $xslname );
+        }
+        else
+        {
+            $path = Phing::getResourcePath("phing/etc/$xslname");
+            
+            if ($path === NULL)
+            {
+                $path = Phing::getResourcePath("etc/$xslname");
+
+                if ($path === NULL)
+                {
+                    throw new BuildException("Could not find $xslname in resource path");
+                }
+            }
+            
+            $file = new PhingFile($path);
+        }
+
+        if (!$file->exists())
+        {
+            throw new BuildException("Could not find file " . $file->getPath());
+        }
+
+        return $file;
+    }
+
 
 
     public function setFile( $filename )
     {
         $this->_file = $filename;
+    }
+    
+    public function setUri( $uri )
+    {
+        $this->_uri = $uri;
     }
 
     public function setProfile( $profile )
@@ -65,5 +134,10 @@ class CssValidate extends Task
     public function setValidatorUrl( $validatorUrl )
     {
         $this->_validatorUrl = $validatorUrl;
+    }
+    
+    public function setDestDir( $destDir )
+    {
+        $this->_destDir = $destDir;
     }
 }
